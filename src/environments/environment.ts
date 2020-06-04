7878f12f5c22be23781c8eb4e80f60a5dc035c33
Michael Frio <michael.frio@dcsg.com>
2020-06-04 08:15:32 -0400

    Added route for POManager

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  portalLinks: [
+    {
+      name: 'POManager',
+      url: 'https://pomanager-ui-dev.apps.vn01.pcf.dcsg.com/',
+    },
+  ],
 };
 
 /*
