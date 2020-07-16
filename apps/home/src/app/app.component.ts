e3b4b2e689af0415522a6f63298b77aaf433531a
Vargo, Adrian <Adrian.Vargo@dcsg.com>
2020-07-16 10:23:17 -0400

    Initial commit for new nx workspace

---
+++ apps/home/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mpe-merch-workbench-ui-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  public title = 'home';
+}
