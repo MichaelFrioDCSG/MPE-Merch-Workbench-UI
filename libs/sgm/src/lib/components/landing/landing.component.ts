d482a83e44f3587f3bf7f1ad23eb31cc3d2cce98
Michael Frio <michael.frio@dcsg.com>
2020-08-13 10:38:06 -0400

    Removed dead code + added public to variables/methods

--- libs/sgm/src/lib/components/landing/landing.component.ts
+++ libs/sgm/src/lib/components/landing/landing.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { AgGridAngular } from 'ag-grid-angular';
-import { Observable } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { IProductHierarchy } from 'libs/shared/src/lib/models/IProductHierarchy';
 import { StoreGroupService } from '../../services/store-group.service';
@@ -13,11 +11,11 @@ import { ImportStoreGroupDialogComponent } from 'libs/shared/src/lib/dialogs/imp
   styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent implements OnInit {
-  @ViewChild('agGrid', { static: false }) agGrid: AgGridAngular;
+  @ViewChild('agGrid', { static: false }) public agGrid: AgGridAngular;
   public selectedData: any;
-  title = 'MPE-SGM';
-  rowData: IProductHierarchy[] = [];
-  columnDefs = [
+  public title = 'MPE-SGM';
+  public rowData: IProductHierarchy[] = [];
+  public columnDefs = [
     {
       headerName: '',
       width: 40,
@@ -32,39 +30,16 @@ export class LandingComponent implements OnInit {
     { headerName: 'Last Modified By', field: 'lastModifiedBy', sortable: true, filter: true },
   ];
 
-  constructor(private http: HttpClient, private dialog: MatDialog, private storeGroupService: StoreGroupService) {}
+  constructor(private dialog: MatDialog, private storeGroupService: StoreGroupService) {}
 
-  ngOnInit() {
-    /*
-    this.getJSON().subscribe(data => {
-      this.rowData = data.storeclusters;
-      this.rowData.map(product => {
-        product.departmentNumber = product.dept_nbr.toString();
-        product.subDepartmentNumber = `${product.dept_nbr.toString()}.${product.subdept_nbr.toString().padStart(3, '0')}`;
-        product.classNumber = `${product.dept_nbr.toString()}.${product.subdept_nbr
-          .toString()
-          .padStart(3, '0')}.${product.class_nbr.toString().padStart(3, '0')}`;
-        product.subClassNumber = `${product.product_grouping} - ${product.sub_class_description}`;
-
-        product.departmentString = `${product.departmentNumber} - ${product.department_description}`;
-        product.subDepartmentString = `${product.subDepartmentNumber} - ${product.sub_department_description}`;
-        product.classString = `${product.classNumber} - ${product.class_description}`;
-        product.subClassString = `${product.product_grouping} - ${product.sub_class_description}`;
-      });
-      console.log(this.rowData);
-    });
-    */
+  public ngOnInit() {
     this.getStoreGroupHeaders();
   }
 
-  getSelectedRows() {
+  public getSelectedRows() {
     const selectedNodes = this.agGrid.api.getSelectedNodes();
     this.selectedData = JSON.stringify(selectedNodes.map(node => node.data));
     // const selectedDataStringPresentation = selectedData.map(node => node.make + ' ' + node.model).join(', ');
-  }
-
-  public getJSON(): Observable<any> {
-    return this.http.get('../assets/store-group-data.json');
   }
 
   public getStoreGroupHeaders() {
