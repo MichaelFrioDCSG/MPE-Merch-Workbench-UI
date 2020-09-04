1892ce015759e001dbaf209d840995efc0aadfa8
Will Reichl <William.Reichl@dcsg.com>
2020-09-04 06:36:31 -0400

    Fix ng unit tests* Set up karma ci config* Remove incorrect tests (per Mike F)

--- apps/home/src/app/app.component.spec.ts
+++ apps/home/src/app/app.component.spec.ts
@@ -23,17 +23,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'home'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('home');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('p').textContent).toContain('Portal APP');
-  });
 });
