e9b477fa7d0604baf0d8c77cbc99d47d95980ac5
Adrian Vargo <adrian.vargo@dcsg.com>
2020-09-17 15:18:49 -0400

    PUR-849 - Replaced individual import of material modules with the custom materialmodule that include them all

--- libs/sgm/src/lib/sgm.module.ts
+++ libs/sgm/src/lib/sgm.module.ts
@@ -1,36 +1,24 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
-import { LandingComponent } from './components/landing/landing.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AgGridModule } from 'ag-grid-angular';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { MaterialModule } from '@mpe/material';
+
+import StoreGroupMgmtEffects from './store/store-group-mgmt.effects';
+import * as fromStoreGroupMgmt from './store/store-group-mgmt.reducer';
+
 import { ImportStoreGroupDialogComponent } from './dialogs/import-store-group-dialog/import-store-group-dialog.component';
 import { InputSpinnerComponent } from 'libs/shared/src/lib/components/inputs/input-spinner/input-spinner.component';
 import { InputDropdownFilterComponent } from 'libs/shared/src/lib/components/inputs/input-dropdown-filter/input-dropdown-filter.component';
 import { InputMultiselectDropdownComponent } from 'libs/shared/src/lib/components/inputs/input-multiselect-dropdown/input-multiselect-dropdown.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatSelectModule } from '@angular/material/select';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { StoreModule } from '@ngrx/store';
-import * as fromStoreGroupMgmt from './store/store-group-mgmt.reducer';
-import { EffectsModule } from '@ngrx/effects';
-import StoreGroupMgmtEffects from './store/store-group-mgmt.effects';
 import { DetailComponent } from './components/detail/detail.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 export const sgmRoutes: Route[] = [
   { path: ':id', component: DetailComponent },
@@ -42,24 +30,12 @@ export const sgmRoutes: Route[] = [
     CommonModule,
     RouterModule,
     BrowserModule,
-    AgGridModule.withComponents([]),
     HttpClientModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatMenuModule,
-    MatDialogModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatTabsModule,
-    MatSelectModule,
     FormsModule,
     ReactiveFormsModule,
-    MatAutocompleteModule,
-    MatChipsModule,
-    MatCheckboxModule,
-    MatProgressSpinnerModule,
+    MaterialModule,
+    AgGridModule.withComponents([]),
     StoreModule.forFeature(fromStoreGroupMgmt.storeGroupMgmtFeatureKey, fromStoreGroupMgmt.reducer),
     EffectsModule.forFeature([StoreGroupMgmtEffects]),
   ],
@@ -71,7 +47,7 @@ export const sgmRoutes: Route[] = [
     InputDropdownFilterComponent,
     DetailComponent,
   ],
-  exports: [LandingComponent],
+  exports: [LandingComponent, DetailComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SgmModule {}
