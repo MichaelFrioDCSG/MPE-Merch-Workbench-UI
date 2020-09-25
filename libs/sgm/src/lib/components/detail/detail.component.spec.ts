99691187958e610b4a6dd17cb99af97c37c6838c
ChrisDSG <cangelo10@gmail.com>
2020-09-25 08:34:26 -0400

    Add ag-grid  tests for rowGroup, sideBar, toolBars

--- libs/sgm/src/lib/components/detail/detail.component.spec.ts
+++ libs/sgm/src/lib/components/detail/detail.component.spec.ts
@@ -28,4 +28,60 @@ describe('DetailComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Row Group panel is visible', () => {
+    expect(component.rowGroupPanelShow).toEqual('always');
+  });
+
+  it('Side Bar is confugured correctly', () => {
+    const sideBar = component.gridApi.getSideBar();
+    expect(sideBar).toBeTruthy();
+    expect(sideBar.defaultToolPanel).toEqual('columns');
+  });
+
+  it('Side Bar columns tool panel is configured correctly', ()=> {
+    const toolPanels: any[] = component.sideBar.toolPanels;
+    const columnToolBarConfig = toolPanels.find(x => x.id === 'columns');
+    expect(columnToolBarConfig).toBeTruthy();
+    expect(columnToolBarConfig.labelDefault).toEqual('Columns');
+    expect(columnToolBarConfig.suppressRowGroups).toEqual('true');
+    expect(columnToolBarConfig.suppressValues).toEqual('true');
+    expect(columnToolBarConfig.suppressPivots).toEqual('true');
+    expect(columnToolBarConfig.suppressPivotMode).toEqual('true');
+    expect(columnToolBarConfig.suppressSideButtons).toEqual('false');
+    expect(columnToolBarConfig.suppressColumnFilter).toEqual('true');
+    expect(columnToolBarConfig.suppressColumnSelectAll).toEqual('true');
+    expect(columnToolBarConfig.suppressColumnExpandAll).toEqual('true');
+  });
+
+  it('Side Bar filters tool panel is configured correctly', ()=> {
+    const toolPanels: any[] = component.sideBar.toolPanels;
+    const filtersToolBarConfig = toolPanels.find(x => x.id === 'filters');
+    expect(filtersToolBarConfig).toBeTruthy();
+    expect(filtersToolBarConfig.labelDefault).toEqual('Columns');
+    expect(filtersToolBarConfig.suppressRowGroups).toEqual('true');
+    expect(filtersToolBarConfig.suppressValues).toEqual('true');
+    expect(filtersToolBarConfig.suppressPivots).toEqual('true');
+    expect(filtersToolBarConfig.suppressPivotMode).toEqual('true');
+    expect(filtersToolBarConfig.suppressSideButtons).toEqual('false');
+    expect(filtersToolBarConfig.suppressColumnFilter).toEqual('true');
+    expect(filtersToolBarConfig.suppressColumnSelectAll).toEqual('true');
+    expect(filtersToolBarConfig.suppressColumnExpandAll).toEqual('true');
+  });
+
+  it('Status bar is configured correctly', () => {
+    const statusBar: any [] = component.statusBar;
+    const TotalAndFiltersStatusPanelConfig = statusBar.find(x => x.statusPanel === 'agTotalAndFilteredRowCountComponent');
+    expect(TotalAndFiltersStatusPanelConfig).toBeTruthy();
+    const TotalRowCountStatusPanelConfig = statusBar.find(x => x.statusPanel === 'agTotalRowCountComponent');
+    expect(TotalRowCountStatusPanelConfig).toBeTruthy();
+    const FilteredRowCountStatusPanelConfig = statusBar.find(x => x.statusPanel === 'agFilteredRowCountComponent');
+    expect(FilteredRowCountStatusPanelConfig).toBeTruthy();
+    const SelectedRowCountComponentStatusPanelConfig = statusBar.find(x => x.statusPanel === 'agSelectedRowCountComponent');
+    expect(SelectedRowCountComponentStatusPanelConfig).toBeTruthy();
+    const AggregationComponentStatusPanelConfig = statusBar.find(x => x.statusPanel === 'agAggregationComponent');
+    expect(AggregationComponentStatusPanelConfig).toBeTruthy();
+  });
+
+
 });
