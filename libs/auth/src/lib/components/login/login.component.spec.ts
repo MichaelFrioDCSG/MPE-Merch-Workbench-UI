b93bdca6f813ec6c53b49ea29f234fdbdc73b21f
Michael Frio <michael.frio@dcsg.com>
2020-10-12 11:07:54 -0400

    PUR-991: fixed text, added testing

--- libs/auth/src/lib/components/login/login.component.spec.ts
+++ libs/auth/src/lib/components/login/login.component.spec.ts
@@ -6,6 +6,8 @@ import { LoginComponent } from './login.component';
 import { MSALConfigFactory, MSALAngularConfigFactory } from '@mpe/auth';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import * as AuthSections from '../../store/auth.state';
+
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
@@ -50,5 +52,12 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain user information in html', () => {
+    const startingElement = fixture.nativeElement;
+    const startingElements = startingElement.querySelectorAll('[data-test="userInfo"]');
+    expect(startingElements.length).toEqual(1);
+    expect(startingElements[0].textContent).toEqual('Testing - Testing@dcsg.com');
   });
 });
