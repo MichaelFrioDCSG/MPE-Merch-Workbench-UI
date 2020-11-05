005a10a0d0b8ef33ce96b0abea173c13ca48fee7
Adrian Vargo <adrian.vargo@dcsg.com>
2020-11-05 13:43:50 -0500

    PUR-1089 - Removed unused logout action

--- libs/auth/src/lib/store/auth.actions.ts
+++ libs/auth/src/lib/store/auth.actions.ts
@@ -2,5 +2,4 @@ import { createAction, props } from '@ngrx/store';
 import { IUserProfile } from '../models/IUserProfile';
 
 export const login = createAction('[App] Login');
-export const logout = createAction('[App] Logout');
 export const setUserProfile = createAction('[App] Set User Data', props<{ UserProfile: IUserProfile }>());
