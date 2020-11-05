4f65ef69680ab776862c6e12c7ce0d93e57f8960
Adrian Vargo <57192675+adrianvargo@users.noreply.github.com>
2020-11-05 13:57:02 -0500

    PUR-1089 - Refactored Auth login code (#66)
    * PUR-1089 - Refactored Auth login code
    
    * PUR-1089 - Removed unused logout action

--- apps/home/src/app/app.component.ts
+++ apps/home/src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { MsalService } from '@azure/msal-angular';
 import { Store } from '@ngrx/store';
-import { IAuthState } from 'libs/auth/src/lib/store/models/IAuthState';
 import * as actions from '../store/actions';
-import * as AuthActions from '@mpe/auth';
 import { IAppState } from '../store/state';
-
 @Component({
   selector: 'mpe-merch-workbench-ui-root',
   templateUrl: './app.component.html',
@@ -13,21 +9,9 @@ import { IAppState } from '../store/state';
 })
 export class AppComponent implements OnInit {
   public title = 'home';
-  constructor(private store: Store<IAppState>, private storeAuth: Store<IAuthState>, private _msalService: MsalService) {}
+  constructor(private store: Store<IAppState>) {}
 
   public ngOnInit(): void {
     this.store.dispatch(actions.applicationLoading());
-    const account = this._msalService.getAccount();
-    if (account) {
-      this.storeAuth.dispatch(
-        AuthActions.setUserProfile({
-          UserProfile: {
-            name: account.name,
-            roles: Array.from(account.idToken.roles),
-            username: account.userName,
-          },
-        })
-      );
-    }
   }
 }
--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -5,9 +5,17 @@ import { StoreModule } from '@ngrx/store';
 import * as AuthReducer from './store/auth.reducers';
 import { BrowserModule } from '@angular/platform-browser';
 import { MsalModule } from '@azure/msal-angular';
+import { EffectsModule } from '@ngrx/effects';
+import AuthEffects from './store/auth.effects';
 
 @NgModule({
-  imports: [CommonModule, BrowserModule, StoreModule.forFeature(AuthReducer.authReducerKey, AuthReducer.reducer), MsalModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    StoreModule.forFeature(AuthReducer.authReducerKey, AuthReducer.reducer),
+    EffectsModule.forFeature([AuthEffects]),
+    MsalModule,
+  ],
   declarations: [LoginComponent],
   exports: [MsalModule],
 })
