8aa77d479435ade68559e8979e910b691e576c99
Adrian Vargo <adrian.vargo@dcsg.com>
2020-11-06 11:29:36 -0500

    PUR-1089 - WiP replacing msal 1.x with msal-browser 2.x

--- libs/auth/src/lib/components/code/code.component.ts
+++ libs/auth/src/lib/components/code/code.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import * as msal from '@azure/msal-browser';
+import { environment } from '@mpe/home/src/environments/environment';
 
 @Component({
   selector: 'mpe-code',
@@ -13,10 +14,33 @@ export class CodeComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
   public ngOnInit(): void {
+    const msalConfig = {
+      auth: {
+        clientId: environment.clientId,
+        authority: environment.authority,
+        redirectUri: environment.redirectUrl,
+      },
+    };
+
+    this.msalInstance = new msal.PublicClientApplication(msalConfig);
+    // this.msalInstance
+    //   .handleRedirectPromise()
+    //   .then(tokenResponse => {
+    //     // Check if the tokenResponse is null
+    //     // If the tokenResponse !== null, then you are coming back from a successful authentication redirect.
+    //     // If the tokenResponse === null, you are not coming back from an auth redirect.
+    //     console.log('AppComponent.handleRedirectPromise:Success', tokenResponse);
+    //   })
+    //   .catch(error => {
+    //     // handle error, either in the library or coming back from the server
+    //     console.log('AppComponent.handleRedirectPromise:Error', error);
+    //   });
+
     const routeFragment: Observable<string> = this.activatedRoute.fragment;
     routeFragment.subscribe(fragment => {
       let token: string = fragment.match(/^(.*?)&/)[1].replace('code=', '');
-      sessionStorage.setItem('code', token);
+      // sessionStorage.setItem('code', token);
+      const myAccounts: msal.AccountInfo[] = this.msalInstance.getAllAccounts();
     });
 
     // try {
