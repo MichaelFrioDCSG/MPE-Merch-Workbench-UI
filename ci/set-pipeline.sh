8b5bac42f3ad5f89ee68acd8c0734f68b4e4f364
dcsg0550306 <john.jankowski@dcsg.com>
2020-12-15 15:31:56 -0500

    PUR-1092: WIP

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
+PROJECT="${1}"
+ENV="${2}"
+shift 2
+
+# notify concourse of a pipeline update
+fly -t $CONCOURSE_TARGET up --pipeline $PIPELINE_NAME
+
+CONCOURSE_TARGET="merchandise-planning-and-execution"
+PIPELINE_ENV="${ENV:=dev}"
+PIPELINE_NAME="${PROJECT}-${PIPELINE_ENV}"
+PIPELINE_CONFIG_FILE="projects/${PROJECT}/pipeline.yml"
+VARS_FILE="projects/${PROJECT}/vars/${PIPELINE_ENV}.yml"
+
 # set pipeline
-fly --target merchandise-planning-and-execution set-pipeline --config pipeline.yml --pipeline mpe-merch-workbench \
+fly --target $CONCOURSE_TARGET set-pipeline \
+    --config $PIPELINE_CONFIG_FILE \
+    --pipeline $PIPELINE_NAME \
+    --load-vars-from $VARS_FILE \
     ${*}
 
 # notify concourse of a pipeline update
-fly -t merchandise-planning-and-execution up --pipeline mpe-merch-workbench
+fly -t $CONCOURSE_TARGET up --pipeline $PIPELINE_NAME
