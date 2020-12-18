be5bf269a3735edec3dcf8935f725d3ac8a7a2ae
dks-johnjankowski <67923069+dks-johnjankowski@users.noreply.github.com>
2020-12-18 14:59:00 -0500

    Feature/pur 989 automate population of link subclasses (#70)
    * PUR-989: Link Subclass population
    
    * PUR-989: Beginning of Tests
    
    * PUR-989: Front End Multiselect filtering implementing filtering for link subclasses and lead subclass with testing
    
    * PUR-989: Changes to pass linting
    
    * PUR-989: Reset of link dropdowns upon lead subclass or assortmentperiod change. Concatination of user selected linksubclasses and getlinksubclasses subclasses sent for POST
    
    * PUR-989: Disable linked subclasses if there is no value for lead subclass
    
    * PUR-989: Disable ALL link dropdowns if there is no lead subclass
    
    * PUR-989: Update ILinkSubclass to reflect full link product hierarchy now present in backend. Link Department, SubDepartment, and Class are now filtered to accurately lead to LinkSubclass values. Tests are updated to reflect updated ILinkSubclass. Loading animation now correctly displays for loading instances of Assortment Period, Lead Subclass, and Link Department
    
    * PUR-989: Update to remove erraneous API call.
    
    * PUR-989: Update cascading clears on link dropdowns
    
    * PUR-989: WIP, MIKE REVIEW
    
    * PUR-989: WIP (merging Adrian's changes to master). Everything is functional at this point.
    
    * PUR-989: Update fiters to use startsWith as opposed to includes for more intuitive usage and readability

---
+++ libs/sgm/src/lib/models/ILinkSubclass.ts
@@ -0,0 +1,17 @@
+export interface ILinkSubclass {
+    assortmentPeriodId: string;
+    copyFromSubclassId: string;
+    copyFromSubclassDisplay: string;
+    departmentId: string;
+    departmentDesc: string;
+    departmentDisplay: string;
+    subDepartmentId: string;
+    subDepartmentDesc: string;
+    subDepartmentDisplay: string;
+    classId: string;
+    classDesc: string;
+    classDisplay: string;
+    subClassId: string;
+    subClassDesc: string;
+    subClassDisplay: string;
+}
