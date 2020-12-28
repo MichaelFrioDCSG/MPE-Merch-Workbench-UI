0e8bd1dc7262552e42a31ebc60176431499b192d
dcsg0550306 <john.jankowski@dcsg.com>
2020-12-28 11:20:02 -0500

    changed dev to be true as opposed to prod

--- apps/home/src/environments/environment.ts
+++ apps/home/src/environments/environment.ts
@@ -5,9 +5,9 @@ import agGridLicense from './agGridLicense';
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   qa: false,
-  dev: false,
+  dev: true,
   uri: 'https://apreportapi-dev.apps.vn01.pcf.dcsg.com/',
   mpe_api: `https://mpe-asmtmgmtservice.apps.vn01.pcf.dcsg.com`,
   clientId: '9323662a-5d5c-4795-a651-37f3b9e15164',
