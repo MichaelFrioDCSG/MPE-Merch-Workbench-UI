37cd7f50dd82c2ce197bff414829c2aa207a1f86
dks-johnjankowski <67923069+dks-johnjankowski@users.noreply.github.com>
2020-12-28 11:25:02 -0500

    Feature/qa stage pipelines (#82)
    * PUR-1139: WIP
    
    * Feature/pur 952 pl attributes import view edit (#73)
    
    * PUR-952 / PUR-1079 & PUR-1075
    
    * PUR-952 PUR-1078
    
    UI - Add new columns for each of the PL Attributes
    
    * PUR-952 PUR-1078
    
    Fixed some type errors when adding dynamic columns
    
    * PUR-952 PUR-1076 PUR-1080
    
    Added ability to edit new PL Attribute columns
    
    * PUR-952 PUR-1076 PUR-1080
    
    Updated PL Attributes header names to be upper case
    Updated referential "Climate" header name to be "Company Climate"
    Added blank value for PL Attributes to be able to remove values
    Added functionality to remove the related PL Attribute when blank is selected as the value
    
    * PUR-952 / PUR-1074
    
    * PUR-952 / PUR-1111
    
    UI - Enable editing after Save / Revert
    
    * PUR-952 PUR-1074
    
    API - Update Save Process
    
    * PUR-952
    
    Lint & Test fixes
    
    * PUR-952
    
    Changed the test:all script to only target specific projects
    
    * PUR-952
    
    Updates from PR
    
    * PUR-952
    
    Added local host configuraion can be used with this command "nx serve --c=local"
    
    * PUR-952
    
    Removed duplicate env api key "mpe_asmtmgmtservice"
    
    * PUR-1092: WIP
    
    * WIP: All environments populate in a single pipeline. Will separate to give each environment its own pipeline
    
    * PUR-1092: WIP. Added Adrian's patch to include environments within the project to be added to scripts (no successful local implementations yet)
    
    * Changes to build paths to reflect proper environments
    
    * changed dev to be true as opposed to prod
    
    * Obliterate vars file
    
    Co-authored-by: Adrian Vargo <57192675+adrianvargo@users.noreply.github.com>

--- apps/home/src/environments/environment.ts
+++ apps/home/src/environments/environment.ts
@@ -1,3 +1,5 @@
+import agGridLicense from './agGridLicense';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
@@ -10,11 +12,8 @@ export const environment = {
   mpe_api: `https://mpe-asmtmgmtservice.apps.vn01.pcf.dcsg.com`,
   clientId: '9323662a-5d5c-4795-a651-37f3b9e15164',
   authority: 'https://login.microsoftonline.com/e04b15c8-7a1e-4390-9b5b-28c7c205a233/',
-  // TODO: Create dev environment and replace
-  //redirectUrl: 'http://localhost:4200/login',
-  redirectUrl: 'https://mpe-merch-workbench.apps.vn01.pcf.dcsg.com/login',
-  agGridLicense:
-    'CompanyName=DCSG,LicensedApplication=Merchandise Workbench,LicenseType=SingleApplication,LicensedConcurrentDeveloperCount=7,LicensedProductionInstancesCount=0,AssetReference=AG-011333,ExpiryDate=21_October_2021_[v2]_MTYzNDc3MDgwMDAwMA==09f1bbb6a68890636bc50bb7eefdbc30',
+  redirectUrl: 'http://localhost:4200',
+  agGridLicense,
 };
 
 /*
