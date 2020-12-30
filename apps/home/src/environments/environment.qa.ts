475a37882877ca23c818f45315c839ededd9fa1f
dks-johnjankowski <67923069+dks-johnjankowski@users.noreply.github.com>
2020-12-30 14:54:06 -0500

    Develop (#88)
    * PUR-1092: Updated API routes in environments (#86)
    
    * PUR-1092: Updated API routes in environments
    
    * PUR-1092: Update API routes to meet PR change requests in Dev
    
    * Feature/qa stage pipelines (#87)
    
    * PUR-1092: Updated API routes in environments
    
    * PUR-1092: Update API routes to meet PR change requests in Dev
    
    * PUR-1092: Change redirect URIs to match new UI pipeline
    
    Co-authored-by: ChrisAngelopoulos <65730108+dks035152@users.noreply.github.com>

--- apps/home/src/environments/environment.qa.ts
+++ apps/home/src/environments/environment.qa.ts
@@ -5,9 +5,9 @@ export const environment = {
   qa: true,
   dev: false,
   uri: 'https://mpe-asmtmgmtclient.apps.vn01.pcf.dcsg.com/',
-  mpe_api: `https://mpe-asmtmgmtservice.apps.vn01.pcf.dcsg.com`,
+  mpe_api: `https://merch-workbench-api-stage.apps.vn01.pcf.dcsg.com`,
   clientId: '9323662a-5d5c-4795-a651-37f3b9e15164',
   authority: 'https://login.microsoftonline.com/e04b15c8-7a1e-4390-9b5b-28c7c205a233/',
-  redirectUrl: 'https://mpe-merch-workbench.apps.vn01.pcf.dcsg.com/login',
+  redirectUrl: 'https://merch-workbench-ui-stage.apps.vn01.pcf.dcsg.com/login',
   agGridLicense,
 };
