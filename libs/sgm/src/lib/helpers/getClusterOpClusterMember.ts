0c7b3bbd72f3919d808eef7a4a9a209ba51a8b8c
Will Reichl <willreichl@gmail.com>
2021-03-01 10:57:40 -0500

    PUR-822: Fix cluster location property name to match updated API model
    * This was causing mapping issues in dev

--- libs/sgm/src/lib/helpers/getClusterOpClusterMember.ts
+++ libs/sgm/src/lib/helpers/getClusterOpClusterMember.ts
@@ -5,11 +5,11 @@ export function getClusterOpClusterMember(
   attributes: IProductLocationAttribute[],
   chain: string,
   tier: string,
-  productLocationAttributes: IClusterLocationProductLocationAttributeValue[]
+  clusterLocationAttributes: IClusterLocationProductLocationAttributeValue[]
 ) {
   const attributeValueArray = attributes
     // filter out any attributes that are not part of the cluster location
-    .filter(attr => productLocationAttributes.find(pla => pla.productLocationAttributeId === attr.id))
+    .filter(attr => clusterLocationAttributes.find(pla => pla.productLocationAttributeId === attr.id))
     // order them by display sequece to standardize the op cluster member order
     .sort((a, b) => a.displaySequence - b.displaySequence)
     // find the pl attribute value based on the value id
@@ -17,7 +17,7 @@ export function getClusterOpClusterMember(
       attr =>
         attr.values.find(
           attrValue =>
-            attrValue.id === productLocationAttributes.find(pla => pla.productLocationAttributeId === attr.id).productLocationAttributeValueId
+            attrValue.id === clusterLocationAttributes.find(pla => pla.productLocationAttributeId === attr.id).productLocationAttributeValueId
         ).value
     );
 
