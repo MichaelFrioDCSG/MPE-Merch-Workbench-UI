4068ceed1aded90e762e40bb2dde63a5d74ce272
Adrian Vargo <57192675+adrianvargo@users.noreply.github.com>
2021-03-01 10:08:18 -0500

    Add Rum Runner to Workbench (#140)
    * Added rumrunner code and new service
    
    * Added Rum Runner module to Home App
    
    * Added Rum Runner user login and open import dialog events

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RumRunnerServiceModule } from '@mpe/rum-runner-service';
 import { LoginComponent } from './components/login/login.component';
 import { StoreModule } from '@ngrx/store';
 import * as AuthReducer from './store/auth.reducers';
 import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-  imports: [CommonModule, BrowserModule, StoreModule.forFeature(AuthReducer.authReducerKey, AuthReducer.reducer)],
+  imports: [CommonModule, BrowserModule, RumRunnerServiceModule, StoreModule.forFeature(AuthReducer.authReducerKey, AuthReducer.reducer)],
   declarations: [LoginComponent],
 })
 export class AuthModule {}
